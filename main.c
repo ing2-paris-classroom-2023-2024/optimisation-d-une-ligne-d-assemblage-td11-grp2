937e6b559e06ff00ed96fb808e56d7c91524cb5f
Mathis-Brt <mathis.bretonne@gmail.com>
2023-11-26 19:25:12 +0100

    Addition main et test code

---
+++ main.c
@@ -0,0 +1,28 @@
+//
+// Created by Mathis Bretonne on 26/11/2023.
+//
+#include <stdio.h>
+
+#include "sspg.c"
+#include "donnees.h"
+
+int main() {
+    // Déclaration des variables
+    int nombre1, nombre2, somme;
+
+    // Demande à l'utilisateur d'entrer le premier nombre
+    printf("Entrez le premier nombre : ");
+    scanf("%d", &nombre1);
+
+    // Demande à l'utilisateur d'entrer le deuxième nombre
+    printf("Entrez le deuxième nombre : ");
+    scanf("%d", &nombre2);
+
+    // Calcul de la somme
+    somme = nombre1 + nombre2;
+
+    // Affichage du résultat
+    printf("La somme de %d et %d est : %d\n", nombre1, nombre2, somme);
+
+    return 0;
+}
